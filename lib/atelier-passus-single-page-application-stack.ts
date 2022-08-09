c0316a4b108cce9bdf79073cc8bf73a792560d8c
amaurs <amaury.gtz@gmail.com>
2022-08-08 18:32:45 -0700

    👍🏼 Setting up configurations that worked for other website.

--- lib/atelier-passus-single-page-application-stack.ts
+++ lib/atelier-passus-single-page-application-stack.ts
@@ -3,8 +3,10 @@ import { aws_s3 as s3 } from 'aws-cdk-lib';
 import { aws_s3_deployment as s3_deployment } from 'aws-cdk-lib';
 import { aws_iam as iam } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { Function, InlineCode, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { aws_certificatemanager as certificate_manager } from 'aws-cdk-lib';
 import { aws_cloudfront as cloudfront } from 'aws-cdk-lib';
+import { aws_route53 as route53 } from 'aws-cdk-lib';
+import { aws_route53_targets as route53_targets } from 'aws-cdk-lib';
 import { aws_codebuild as codebuild } from 'aws-cdk-lib';
 import * as path from 'path';
 
@@ -13,14 +15,15 @@ export class AtelierPassusSinglePageApplicationStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
+        const primaryDomain = 'passus-atelier'
+
         const bucket = new s3.Bucket(this, "AtelierPassusSPABucket", {
             websiteIndexDocument: 'index.html',
             websiteErrorDocument: 'index.html',
             removalPolicy: cdk.RemovalPolicy.DESTROY,
-            bucketName: 'www.atelier-passus.com',
+            bucketName: primaryDomain,
         });
 
-        console.log('Giving it access');
         const cloudFrontOAI = new cloudfront.OriginAccessIdentity(this, 'OAI', {
             comment: `OAI for Atelier Passus website.`,
         });
@@ -36,18 +39,35 @@ export class AtelierPassusSinglePageApplicationStack extends cdk.Stack {
           cloudFrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId
         );
 
+        const hostedZone = new route53.HostedZone(this, 'AtelierPassusHostedZone', {
+            zoneName: primaryDomain,
+        });
+
+        const certificate = new certificate_manager.Certificate(this, 'AtelierPassusCertificate', {
+            domainName: primaryDomain,
+            validation: certificate_manager.CertificateValidation.fromDns(hostedZone),
+        });
+
+        bucket.addToResourcePolicy(cloudfrontS3Access);
 
         const cloudFrontDistProps: cloudfront.CloudFrontWebDistributionProps = {
-          viewerCertificate: cloudfront.ViewerCertificate.fromCloudFrontDefaultCertificate('www.atelier-passus.com'),
-          originConfigs: [
-            {
-              s3OriginSource: {
-                s3BucketSource: bucket,
-                originAccessIdentity: cloudFrontOAI,
-              },
-              behaviors: [{ isDefaultBehavior: true }],
-            },
-          ],
+            originConfigs: [
+                {
+                    s3OriginSource: {
+                        s3BucketSource: bucket,
+                        originAccessIdentity: cloudFrontOAI,
+                    },
+                    behaviors: [{ isDefaultBehavior: true }],
+                },
+            ],
+            viewerCertificate: {
+                aliases: [primaryDomain],
+                props: {
+                    acmCertificateArn: certificate.certificateArn,
+                    sslSupportMethod: 'sni-only',
+                    minimumProtocolVersion: 'TLSv1.1_2016',
+                }
+            }
         };
 
         const cloudfrontDist = new cloudfront.CloudFrontWebDistribution(
@@ -56,9 +76,12 @@ export class AtelierPassusSinglePageApplicationStack extends cdk.Stack {
           cloudFrontDistProps
         );
 
-        bucket.addToResourcePolicy(cloudfrontS3Access);
+        new route53.ARecord(this, 'AtelierPassusAlias', {
+            zone: hostedZone,
+            target: route53.RecordTarget.fromAlias(new route53_targets.CloudFrontTarget(cloudfrontDist)),
+        });
 
-        new s3_deployment.BucketDeployment(this, 'DeployWebsite', {
+        new s3_deployment.BucketDeployment(this, 'AtelierPassusDeployment', {
             sources: [s3_deployment.Source.asset(path.join(__dirname, '../atelier-passus/build'))],
             destinationBucket: bucket,
             memoryLimit: 512,
