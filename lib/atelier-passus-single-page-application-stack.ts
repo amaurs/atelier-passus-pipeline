b9b89bdea28947b9b59a8711bbe697324b77e889
amaurs <amaury.gtz@gmail.com>
2022-08-08 19:11:08 -0700

    ❌ Removing hostname configuration.

--- lib/atelier-passus-single-page-application-stack.ts
+++ lib/atelier-passus-single-page-application-stack.ts
@@ -25,9 +25,8 @@ export class AtelierPassusSinglePageApplicationStack extends cdk.Stack {
         });
 
         const cloudFrontOAI = new cloudfront.OriginAccessIdentity(this, 'OAI', {
-            comment: `OAI for Atelier Passus website.`,
+            comment: `Origin Access Identity for Atelier Passus website.`,
         });
-
 
         const cloudfrontS3Access = new iam.PolicyStatement();
         cloudfrontS3Access.addActions('s3:GetBucket*');
@@ -38,15 +37,6 @@ export class AtelierPassusSinglePageApplicationStack extends cdk.Stack {
         cloudfrontS3Access.addCanonicalUserPrincipal(
           cloudFrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId
         );
-
-        const hostedZone = new route53.HostedZone(this, 'AtelierPassusHostedZone', {
-            zoneName: primaryDomain,
-        });
-
-        const certificate = new certificate_manager.Certificate(this, 'AtelierPassusCertificate', {
-            domainName: primaryDomain,
-            validation: certificate_manager.CertificateValidation.fromDns(hostedZone),
-        });
 
         bucket.addToResourcePolicy(cloudfrontS3Access);
 
@@ -60,26 +50,13 @@ export class AtelierPassusSinglePageApplicationStack extends cdk.Stack {
                     behaviors: [{ isDefaultBehavior: true }],
                 },
             ],
-            viewerCertificate: {
-                aliases: [primaryDomain],
-                props: {
-                    acmCertificateArn: certificate.certificateArn,
-                    sslSupportMethod: 'sni-only',
-                    minimumProtocolVersion: 'TLSv1.1_2016',
-                }
-            }
         };
 
         const cloudfrontDist = new cloudfront.CloudFrontWebDistribution(
-          this,
-          `AtelierPassusConfiguration`,
-          cloudFrontDistProps
+            this,
+            `AtelierPassusConfiguration`,
+            cloudFrontDistProps
         );
-
-        new route53.ARecord(this, 'AtelierPassusAlias', {
-            zone: hostedZone,
-            target: route53.RecordTarget.fromAlias(new route53_targets.CloudFrontTarget(cloudfrontDist)),
-        });
 
         new s3_deployment.BucketDeployment(this, 'AtelierPassusDeployment', {
             sources: [s3_deployment.Source.asset(path.join(__dirname, '../atelier-passus/build'))],
